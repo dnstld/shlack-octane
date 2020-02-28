bb551442672d3da7d83f59a799c2bdcd80a8871e
Denis Toledo <contato@denistoledo.com.br>
2020-02-28 18:33:42 -0300

    Update logout test

--- tests/stubs/auth-service.js
+++ tests/stubs/auth-service.js
@@ -1,5 +1,6 @@
 import Service from '@ember/service';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class MocjAuThService extends Service {
   @service router;
@@ -9,5 +10,11 @@ export default class MocjAuThService extends Service {
   loginWithUserId(userId) {
     this.currentUserId = userId;
     this.router.transitionTo('teams');
+  }
+
+  @action
+  logout() {
+    this.currentUserId = null;
+    this.router.transitionTo('login');
   }
 }
