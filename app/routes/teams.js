d64c1b08b1ebe9c8a6ba1411f9b2d3c75d5fd031
Denis Toledo <contato@denistoledo.com.br>
2020-03-08 17:01:47 -0300

    Use hard coded data

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -1,6 +1,27 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
+export const ALL_TEAMS = [
+  {
+    "id": "linkedin",
+    "name": "LinkedIn",
+    "order": 2,
+    "iconUrl": "/assets/img/linkedin.png"
+  },
+  {
+    "id": "ms",
+    "name": "Microsoft",
+    "order": 3,
+    "iconUrl": "/assets/img/microsoft.png"
+  },
+  {
+    "id": "avengers",
+    "name": "Avengers",
+    "order": 4,
+    "iconUrl": "/assets/img/avengers.jpg"
+  },
+];
+
 export default class TeamsRoute extends Route {
   @service auth;
 
@@ -10,5 +31,9 @@ export default class TeamsRoute extends Route {
     if (!this.auth.currentUserId) {
       this.transitionTo('login');
     }
+  }
+
+  async model() {
+    return ALL_TEAMS;
   }
 }
