57d0f0f17d9186fd8e059f30a5a46f6afd1493a2
Denis Toledo <contato@denistoledo.com.br>
2020-03-19 09:05:07 -0300

    send messa via POST

--- app/components/chat-container.js
+++ app/components/chat-container.js
@@ -1,9 +1,12 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import fetch from 'fetch';
 
 export default class ChatContainerComponent extends Component {
+  @service auth;
+
   @tracked
   messages = [];
 
@@ -15,5 +18,27 @@ export default class ChatContainerComponent extends Component {
 
     const resp = await fetch(`/api/teams/${teamId}/channels/${id}/messages`);
     this.messages = await resp.json();
+  }
+
+  @action
+  async createMessage(body) {
+    const userId = this.auth.currentUserId;
+
+    const {
+      channel: { id: channelId, teamId },
+    } = this.args;
+
+    const resp = await fetch('/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        teamId,
+        channelId,
+        userId,
+        body
+      })
+    });
   }
 }
